6f69fcafd8bfd0009814feaf4344cc16a03ad4ed
JackN9 <150101016+JackN9@users.noreply.github.com>
2024-10-20 18:37:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
-import React from 'react'
-import MainLayout from './layouts/MainLayout'
+// import React from 'react'
+// import MainLayout from './layouts/MainLayout'
+// import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+// import ErrorPage from './pages/ErrorPage';
+// import HomePage from './pages/HomePage';
+// import BucketPage from './pages/BucketPage';
+// import CalendarPage from './pages/CalendarPage';
+// import TodoPage from './pages/TodoPage';
+
+// const App = () => {
+  
+//   // Pages Routes
+//   const router = createBrowserRouter(
+//     createRoutesFromElements(
+//       <Route path='/' element={<MainLayout />}>
+//         <Route index element={<HomePage />} />
+//         <Route path='/buckets/:bucket_name' element={<BucketPage />}>
+//           <Route path=':todo_id' element={<TodoPage />}/>
+//         </Route>
+//         <Route path='/calendar' element={CalendarPage}/>
+//         <Route path='*' element={<ErrorPage />} />
+//       </Route>
+//     )
+//   );
+
+
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import MainLayout from './layouts/MainLayout';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
@@ -8,7 +44,7 @@ import CalendarPage from './pages/CalendarPage';
 import TodoPage from './pages/TodoPage';
 
 const App = () => {
-  
+
   // Pages Routes
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -17,12 +53,12 @@ const App = () => {
         <Route path='/buckets/:bucket_name' element={<BucketPage />}>
           <Route path=':todo_id' element={<TodoPage />}/>
         </Route>
-        <Route path='/calendar' element={CalendarPage}/>
+        {/* Add the Calendar Page */}
+        <Route path='/calendar' element={<CalendarPage />} />
         <Route path='*' element={<ErrorPage />} />
       </Route>
     )
   );
-
 
   return (
     <>
@@ -31,4 +67,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
