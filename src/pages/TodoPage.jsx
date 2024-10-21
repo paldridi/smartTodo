78f2e3de163d2378be31ab92882a958cf03f7729
Mohammad Al Dridi <52922034+paldridi@users.noreply.github.com>
2024-10-20 21:03:56 -0400

    Revert "Jack"

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,38 +1,11 @@
-// import React from 'react'
-
-// const TodoPage = () => {
-//   return (
-//     <div>
-//       This is the todo page
-//     </div>
-//   )
-// }
-
-// export default TodoPage
-
-import React from 'react';
-
-// Define and export the tasks array
-export const tasks = [
-  { title: 'Task 1', date: '2024-10-20' },
-  { title: 'Task 2', date: '2024-10-22' },
-  { title: 'Task 3', date: '2024-10-25' },
-];
+import React from 'react'
 
 const TodoPage = () => {
   return (
     <div>
-      <h1>This is the todo page</h1>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task.title} - {task.date}
-          </li>
-        ))}
-      </ul>
+      This is the todo page
     </div>
-  );
-};
+  )
+}
 
-export default TodoPage;
-
+export default TodoPage
